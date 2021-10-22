6a1715ff1f93b8557b0467718b58fc21b347585c
Keegan77 <keegansb77@gmail.com>
2021-10-21 21:36:58 -0400

    Moved spawning to spawnmanager

--- New Unity Project (1)/Assets/Scripts/GameManager.cs
+++ New Unity Project (1)/Assets/Scripts/GameManager.cs
@@ -8,8 +8,8 @@ public class GameManager : MonoBehaviour
     //BATTLEGROUND SCALE 1.481725
     public int level = 1;
     public int enemiesToDefeat;
-    public int playstyle; // 0 = swordsman, 1 = archer, 2 = mage
-    public GameObject[] playstyles;
+    //public int playstyle; // 0 = swordsman, 1 = archer, 2 = mage
+    //public GameObject[] playstyles;
     public GameObject[] player;
     public int playerhealth = 3;
     GameObject door;
@@ -32,9 +32,8 @@ public class GameManager : MonoBehaviour
     void Start()
     {
         door = GameObject.FindGameObjectWithTag("Door");
-        playstyle = Random.Range(0, 3);
-        Debug.Log(playstyle);
-        Instantiate(playstyles[playstyle], new Vector3(-8.57f, 0.468f, 0), transform.rotation);
+        //playstyle = Random.Range(0, 3);
+        //Instantiate(playstyles[playstyle], new Vector3(-8.57f, 0.468f, 0), transform.rotation);
         //Debug.Log("yo");
     }
 
@@ -42,7 +41,7 @@ public class GameManager : MonoBehaviour
     void Update()
     {
         enemiesToDefeat = level;
-        EndGame();
+        //EndGame();
         if (spawnManagerScript.enemiesdead == level)
         {
             Destroy(door.gameObject);
@@ -53,7 +52,7 @@ public class GameManager : MonoBehaviour
         //Debug.Log(playerhealth);
 
     }
-    private void EndGame()
+    /*private void EndGame()
     {
         if (playerhealth <= 0)
         {
@@ -74,5 +73,5 @@ public class GameManager : MonoBehaviour
             }
             
         }
-    }
+    }*/
 }
